4a5792b92e619bc9d2ef1eaf54bad6e820f19d16
Md. Ibn Jihan <101347202+mdibnjihan@users.noreply.github.com>
2022-11-13 17:50:34 +0600

    Updated files for GitHub pages.

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ function closeNav() {
     document.getElementById("sideNav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+function verify() {
+  window.open("https://www.brightskills.com/search-certificate/2z3cvwi5", "_blank", "toolbar=yes,top=500,left=500,width=400,height=400");
+
+}
