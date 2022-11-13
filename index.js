2b3a9fa73f156c16e38b08b659293f25ace79f59
Md. Ibn Jihan <101347202+mdibnjihan@users.noreply.github.com>
2022-11-13 17:00:08 +0600

    Updated files for website
    Organized code for better understanding. A few changes were made. Ready for public release.

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 function openNav() {
-    document.getElementById("mySidebar").style.width = "auto";
-    /* let element = document.getElementById("mySidebar");
-    let applied_styles = window.getComputedStyle(element);
-    let current_width = applied_styles.width;
-    let current_window_width = window.innerWidth;
-    let margin_to_set = current_window_width - 246.281; */
-    // style_applied.width
-    /* document.getElementById("main").style.marginLeft = "0"; */
+    document.getElementById("sideNav").style.width = "auto";
   }
 
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
-  }
+}
